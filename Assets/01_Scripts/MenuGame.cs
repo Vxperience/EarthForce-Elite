9ca2bbdceb49e0a02348ee7bee82638a2192745a
Nicolas <nicolas.chauvigne@yahoo.fr>
2019-11-18 16:24:57 +0100

    First commit
    + Basic Menu
    |- Choice level
    |- Launch game
    + Game
    | - Player

---
+++ Assets/01_Scripts/MenuGame.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuGame : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject Pause;
+    private bool ispause;
+    
+    void Start()
+    {
+        ispause = true;
+    }
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            ispause = false;
+        if (Input.GetMouseButtonUp(0))
+            ispause = true;
+        Player.GetComponent<Player>().ispause = ispause;
+        Pause.SetActive(ispause);
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
